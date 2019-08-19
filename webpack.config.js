1936dced7c6031cef16785fa1cbcd78dbc4b1115
Dmitry <kambulov.dima455@gmail.com>
2019-08-19 17:58:02 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname,'./dist/js'),
+    filename: 'bundle.js',
+    publicPath:'dist/js'
+  },
+
+  devtool: "source-map",
+
+devServer: {
+  overlay: true,
+},
+
+module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+};
