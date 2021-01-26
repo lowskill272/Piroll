a355d71a2ec325367fefcb746f683d017506a529
lowskill272 <kambulov.dima455@gmail.com>
2021-01-26 14:56:17 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,36 @@
 import $ from 'jquery';
 import 'slick-carousel';
-import '../scss/style.scss';  
+import '../scss/style.scss';
 
+var firebaseConfig = {
+    apiKey: "AIzaSyBVDtZsFiYZGPkk8oUdq71J3J6AnCh_32U",
+    authDomain: "test-29c18.firebaseapp.com",
+    projectId: "test-29c18",
+    storageBucket: "test-29c18.appspot.com",
+    messagingSenderId: "73861637376",
+    appId: "1:73861637376:web:5c85b0c88c33ee552f3fe5",
+    measurementId: "G-CWEXC6CLV8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
+const messaging = firebase.messaging();
+
+messaging.getToken({ vapidKey: '<YOUR_PUBLIC_VAPID_KEY_HERE>' }).then((currentToken) => {
+    if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        // ...
+        console.log(currentToken)
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+    }
+}).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+});
 
 function isAnyPartOfElementInViewport(el) {
 
